6680bb283cf220ba1821510963f0e1150eaf73f6
renjiecui <renjiecui@microsoft.com>
2022-08-08 10:25:43 +0800

    init proj

---
+++ MyTestLib/TestDebugWithSymbolPackage.cs
@@ -0,0 +1,28 @@
+﻿namespace MyTestLib
+{
+    public class TestDebugWithSymbolPackage
+    {
+        public TestDebugWithSymbolPackage()
+        {
+            this.StrField = "this is a string";
+
+            this.IntField = 0;
+
+            this.DoubleField = 0.0;
+
+            this.LongField = (long)(int.MaxValue) + 1;
+
+            this.FloatField = (float)(int.MaxValue) + 1;
+        }
+
+        public string StrField { get; set; }
+
+        public int IntField { get; set; }
+
+        public double DoubleField { get; set; }
+
+        public long LongField { get; set; }
+
+        public float FloatField { get; set; }
+    }
+}
